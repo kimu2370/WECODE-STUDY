1fa3a6377557e552674970f6ffa2f9e6cdba0645
seunghyun <51603029+kimu2370@users.noreply.github.com>
2019-12-31 03:24:00 +0900

    update app.js and create moment 19/12/31

--- weatherApp/README.md
+++ weatherApp/README.md
@@ -7,6 +7,19 @@
 
 19/12/30 기록
 
-- timezone api를 통해 timestamp와 timeZoneId, rawOffset을 얻어 옴.
-- rawOffset 데이터 가공과 fetch에 임의로 넣은 timestamp 값
+1. timezone api를 통해 timestamp와 timeZoneId, rawOffset을 얻어 옴.
+2. rawOffset 데이터 가공과 fetch에 임의로 넣은 timestamp 값
   데이터 동적으로 가공하기.
+
+19/12/30 기록2
+- momentjs,moment-timeZone-datejs를 이용해 타임존 데이터를 가공하여 날짜와 시간 표현 성공.
+- TODO : 
+  - forecast api 를 이용해서 예측 데이터 정보 얻어오기.
+  - 날씨별로 맞추어 배경색상을 변경해보기
+    javasciprt를 사용해 element의 내용 뿐만 아니라 element의 style도 변경할 수 있다.
+   
+  - 배경 참고 사이트 https://uigradients.com/
+   
+  - 나만의 날씨 이미지로 변경하기.
+      이미지를 수정하거나, 새로운 이미지를 추가해보자.
+      원하는 이미지를 찾아 img 폴더에 저장 후 data.js 파일을 수정해보자.
--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,3 +1,5 @@
+const date = document.querySelector('.date');
+const time = document.querySelector('.time');
 const icon = document.querySelector(".weatherIcon");
 const temp = document.querySelector(".temperature");
 const city = document.querySelector(".city");
@@ -15,11 +17,13 @@ searchForm.addEventListener("submit", function(event) {
 
    getGeoCode(text)  // text = 입력한 데이터
       .then(function(code) {
-         return getWeatherData(code.lat, code.lng);
+         return getWeatherData(code.lat, code.lng);   //다음 익명함수에 매개변수에 넣을 데이터
       })
       .then(function(info) {
-         setWeatherInfo(text, info);
-         getMomentInfo(info);
+         return getMomentInfo(info);   //다음 익명함수에 매개변수에 넣을 데이터
+      })
+      .then(function(info) {
+         setInfo(text, info); //화면에 표시할 데이터
       })
       .catch(function(err) { // 에러가 발생했을때 뜨게 됨.
          console.log("에러가 발생했습니다.", err);
@@ -34,7 +38,6 @@ function getGeoCode(city) {
          `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${GOOGLE_KEY}`
       ).then(function(response) {
          response.json().then(function(data) {
-            console.log(data);
             let loactionDATA = {};
             loactionDATA.lat = data.results[0]["geometry"]["location"]["lat"];
             loactionDATA.lng = data.results[0]["geometry"]["location"]["lng"];
@@ -45,74 +48,70 @@ function getGeoCode(city) {
 }
 
 // 2. 위도, 경도를 바탕으로 해당 지역의 날씨를 가지고 옵니다.
-/**
- * getGeoCode 함수에서 return한 객체에서 lat, lng 값을 가지고 옵니다.
- * lat - 위도
- * lng - 경도
- */
 function getWeatherData(lat, lng) {
    return new Promise(function(resolve, reject) {
       fetch(
          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${WEATHER_KEY}`
       ).then(function(response) {
          response.json().then(function(data) {
-            let weatherAndlocationInfo = {};
-            weatherAndlocationInfo.temp = data["main"]["temp"];
-            weatherAndlocationInfo.weather = data["weather"][0]["main"];
-            weatherAndlocationInfo.lat = lat;
-            weatherAndlocationInfo.lng = lng;
-            console.log("우리가 받은 날씨 데이터 ", data);
-            resolve(weatherAndlocationInfo); // 여기에 저희가 원하는 형식의 값을 넣어주세요!
+            let appInfo = {};
+            appInfo["temp"] = data["main"]["temp"];
+            appInfo["weather"] = data["weather"][0]["main"];
+            appInfo["lat"] = lat;
+            appInfo["lng"] = lng;
+            appInfo["dt"] = data["dt"];
+            resolve(appInfo); // 온도,날씨,위도,경도,dt 정보를 담는다.
          });
       });
    });
 }
 
-// 3. 저희가 받은 날씨 정보로 화면을 변경합니다.
-/**
- * cityName - 입력한 도시 이름
- * weatherInfo - getWeatherData에서 return해준 객체
- * {
- *    temp: 기온
- *    weather: 날씨
- * }
- */
-function setWeatherInfo(cityName, weatherInfo) {
-   // console.log("cityName",cityName);
-   // console.log("weatherInfo",weatherInfo);
-   // 섭씨->켈빈 °C = K − 273.15
-   temp.innerText = Math.floor(weatherInfo.temp-273.15)+" ℃";
-   const weatherLink = imgLinks[weatherInfo.weather];
-   icon.src = `image/icon/${weatherLink}`; 
-   city.innerText = cityName;
-   input.disabled = false; //입력창을 활성화 시킴.
-}
 
-function getMomentInfo(locationInfo) {
-   console.log("locationInfo",locationInfo);
+
+// 3. 타임존 정보를 얻는다.
+function getMomentInfo(appInfo) {
    return new Promise(function(resolve, reject) {
       fetch(
-         `https://maps.googleapis.com/maps/api/timezone/json?location=${locationInfo.lat},${locationInfo.lng}&timestamp=1577707992&key=${GOOGLE_KEY}`
+         `https://maps.googleapis.com/maps/api/timezone/json?location=${appInfo["lat"]},${appInfo["lng"]}&timestamp=${appInfo["dt"]}&key=${GOOGLE_KEY}`
       ).then(function(response) {
          response.json().then(function(data) {
-            console.log("모먼트 데이터",data);
-            resolve(locationInfo);
+            // console.log("타임존 데이터",data);
+            appInfo["timeZoneId"]=data["timeZoneId"];
+            resolve(appInfo); // 화면에 표시할 정보
          });
       });
    }); 
 }
 
-/**
- * 해볼 것들
- *
- * - 날씨별로 맞추어 배경색상을 변경해보기
- * javasciprt를 사용해 element의 내용 뿐만 아니라 element의 style도 변경할 수 있습니다.
- * 어떻게 javascript를 사용해 css를 다룰 수 있을까요?
- *
- * 배경 참고 사이트 https://uigradients.com/
- *
- * - 나만의 날씨 이미지로 변경하기
- * 이미지를 수정하거나, 새로운 이미지를 추가해보세요.
- * 원하는 이미지를 찾아 img 폴더에 저장 후 data.js 파일을 수정해보세요.
- *
- */
+// 4. 받은 날씨 정보로 화면을 변경합니다.
+function setInfo(cityName, appInfo) {
+   // console.log(appInfo);
+   //moment로 현재 시간 포맷팅
+   const dateTimeFormat = moment().tz(appInfo["timeZoneId"]).format(); 
+
+   //날짜 포맷팅
+   const dateFormat = dateTimeFormat.split('T')[0].split('-');
+   let dateItem = document.createElement('div');
+   dateItem.innerText = dateFormat[0]+"년 "+dateFormat[1]+"월 "+dateFormat[2]+"일";
+   date.append(dateItem);   
+
+   //시간 포맷팅
+   const timeFormat = dateTimeFormat.split('T')[1].substring(0,8).split(':');
+   let timeItem = document.createElement('div');
+   timeItem.innerText = timeFormat[0]+"시 "+timeFormat[1]+"분 ";
+   time.append(timeItem);
+   
+   //날씨 아이콘
+   const weatherLink = imgLinks[appInfo.weather];
+   icon.src = `image/icon/${weatherLink}`; 
+
+   //온도 섭씨->켈빈 °C = K − 273.15
+   temp.innerText = Math.floor(appInfo.temp-273.15)+" ℃";
+
+   //도시 이름
+   city.innerText = cityName;
+
+    //입력창을 활성화 시킴.
+   input.disabled = false;
+}
+
