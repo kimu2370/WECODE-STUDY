d5fe562240237fc4bbd092ff9bbe3bb273d74f8a
seunghyun <51603029+kimu2370@users.noreply.github.com>
2019-12-31 13:25:47 +0900

    update app.js

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -7,7 +7,7 @@ const searchForm = document.getElementById("searchForm");
 const input = document.getElementById("searchText");
 
 // 도시 이름을 입력하고, Enter키를 누루면 아래 함수가 실행됩니다.
-searchForm.addEventListener("submit", function(event) {
+searchForm.addEventListener("submit",(event) => {
    event.preventDefault();
 
    const text = input.value;
@@ -16,16 +16,19 @@ searchForm.addEventListener("submit", function(event) {
    input.disabled = true;
 
    getGeoCode(text)  // text = 입력한 데이터
-      .then(function(code) {
-         return getWeatherData(code.lat, code.lng);   //다음 익명함수에 매개변수에 넣을 데이터
+      .then((code) => {
+         return getWeatherData(code.lat, code.lng);
       })
-      .then(function(info) {
-         return getMomentInfo(info);   //다음 익명함수에 매개변수에 넣을 데이터
+      .then((info) => {
+         return getMomentInfo(info);
       })
-      .then(function(info) {
-         setInfo(text, info); //화면에 표시할 데이터
+      .then((info) => {
+         return getForecastInfo(info);
       })
-      .catch(function(err) { // 에러가 발생했을때 뜨게 됨.
+      .then((info) => {
+         setInfo(text, info); // info = 화면에 표시할 데이터
+      })
+      .catch((err) => { // 에러가 발생했을때 뜨게 됨.
          console.log("에러가 발생했습니다.", err);
          input.disabled = false;
       });
@@ -33,14 +36,17 @@ searchForm.addEventListener("submit", function(event) {
 
 // 1. 입력된 도시의 위도, 경도 값을 가지고 옵니다.
 function getGeoCode(city) {
-   return new Promise(function(resolve, reject) {
+   //Promise resolve를 통해 .then의 익명함수 매개변수에 값을 전달.
+   return new Promise((resolve, reject) => { 
       fetch(
          `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=${GOOGLE_KEY}`
-      ).then(function(response) {
-         response.json().then(function(data) {
+      ).then((response) => {
+         response.json().then((data) => {
+
             let loactionDATA = {};
-            loactionDATA.lat = data.results[0]["geometry"]["location"]["lat"];
-            loactionDATA.lng = data.results[0]["geometry"]["location"]["lng"];
+            loactionDATA.lat = data["results"][0]["geometry"]["location"]["lat"];
+            loactionDATA.lng = data["results"][0]["geometry"]["location"]["lng"];
+
             resolve(loactionDATA);
          });
       });
@@ -49,18 +55,20 @@ function getGeoCode(city) {
 
 // 2. 위도, 경도를 바탕으로 해당 지역의 날씨를 가지고 옵니다.
 function getWeatherData(lat, lng) {
-   return new Promise(function(resolve, reject) {
+   return new Promise((resolve, reject) => {
       fetch(
          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${WEATHER_KEY}`
-      ).then(function(response) {
-         response.json().then(function(data) {
-            let appInfo = {};
+      ).then((response) => {
+         response.json().then((data) => {
+
+            let appInfo = {}; //화면에 보여줄 데이터를 담는다.
             appInfo["temp"] = data["main"]["temp"];
             appInfo["weather"] = data["weather"][0]["main"];
             appInfo["lat"] = lat;
             appInfo["lng"] = lng;
             appInfo["dt"] = data["dt"];
-            resolve(appInfo); // 온도,날씨,위도,경도,dt 정보를 담는다.
+
+            resolve(appInfo); 
          });
       });
    });
@@ -68,38 +76,65 @@ function getWeatherData(lat, lng) {
 
 
 
-// 3. 타임존 정보를 얻는다.
+// 3. 검색한 지도 정보를 바탕으로 타임존 정보를 얻는다.
 function getMomentInfo(appInfo) {
-   return new Promise(function(resolve, reject) {
+   return new Promise((resolve, reject) => {
       fetch(
          `https://maps.googleapis.com/maps/api/timezone/json?location=${appInfo["lat"]},${appInfo["lng"]}&timestamp=${appInfo["dt"]}&key=${GOOGLE_KEY}`
-      ).then(function(response) {
-         response.json().then(function(data) {
+      ).then((response) => {
+         response.json().then((data) => {
+
             // console.log("타임존 데이터",data);
             appInfo["timeZoneId"]=data["timeZoneId"];
+
+            //moment로 객체에 현재 날짜와 시간 추가
+            appInfo["dateFormat"]= moment().tz(appInfo["timeZoneId"]).format().split('T')[0].split('-');
+            appInfo["timeFormat"]= moment().tz(appInfo["timeZoneId"]).format().split('T')[1].substring(0,8).split(':');
+
             resolve(appInfo); // 화면에 표시할 정보
          });
       });
    }); 
 }
 
+// 4. 검색한 지도를 바탕으로 일기예보 정보를 얻는다.
+function getForecastInfo(appInfo) {
+   let listArr=[],forecast=[];
+   return new Promise((resolve, reject) => {
+      fetch(
+         `https://api.openweathermap.org/data/2.5/forecast?lat=${appInfo["lat"]}&lon=${appInfo["lng"]}&APPID=${WEATHER_KEY}`
+      ).then((response) => {
+         response.json().then((data) => {
+
+            // 12:00:00 중 ("12")시에 해당하는 부분들을 배열로 담았다.
+            listArr=data["list"].map(item=>(
+                  item["dt_txt"].split(" ")[1].split(":")[0]
+               )).slice();
+
+            //출력하고 싶은 일기예보 5개만 담았다.(현재시간의 예보를 제외한 다음 예보부터)
+            for(let i=3; i<8;i++){
+               forecast.push(listArr[i]);
+            }
+            appInfo["forecast"] = forecast.slice();
+            console.log(appInfo);
+
+            resolve(appInfo);
+         });
+      });
+   });
+}
+
 // 4. 받은 날씨 정보로 화면을 변경합니다.
 function setInfo(cityName, appInfo) {
    // console.log(appInfo);
-   //moment로 현재 시간 포맷팅
-   const dateTimeFormat = moment().tz(appInfo["timeZoneId"]).format(); 
-
    //날짜 포맷팅
-   const dateFormat = dateTimeFormat.split('T')[0].split('-');
-   let dateItem = document.createElement('div');
-   dateItem.innerText = dateFormat[0]+"년 "+dateFormat[1]+"월 "+dateFormat[2]+"일";
-   date.append(dateItem);   
+   date.innerText = appInfo["dateFormat"][0]+"년 "+
+                     appInfo["dateFormat"][1]+"월 "+
+                     appInfo["dateFormat"][2]+"일";
 
    //시간 포맷팅
-   const timeFormat = dateTimeFormat.split('T')[1].substring(0,8).split(':');
-   let timeItem = document.createElement('div');
-   timeItem.innerText = timeFormat[0]+"시 "+timeFormat[1]+"분 ";
-   time.append(timeItem);
+   time.innerText = appInfo["timeFormat"][0]+"시 "+
+                     appInfo["timeFormat"][1]+"분 ";
    
    //날씨 아이콘
    const weatherLink = imgLinks[appInfo.weather];
