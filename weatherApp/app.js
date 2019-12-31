c8c19783697ae107dbd59b39433cba78fc92ae42
seunghyun <51603029+kimu2370@users.noreply.github.com>
2019-12-31 20:45:09 +0900

    update background and image change 19/12/31

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,10 +1,16 @@
-const date = document.querySelector('.date');
-const time = document.querySelector('.time');
+const body = document.querySelector("body");
+const date = document.querySelector(".date");
+const time = document.querySelector(".time");
+const weatherBox = document.querySelector("#weatherBox");
 const icon = document.querySelector(".weatherIcon");
 const temp = document.querySelector(".temperature");
 const city = document.querySelector(".city");
 const searchForm = document.getElementById("searchForm");
 const input = document.getElementById("searchText");
+const forecastItems = document.querySelectorAll(".forecastItem");
+const forecastIcon = document.querySelectorAll(".forecastIcon");
+const forecastTemp = document.querySelectorAll(".forecastTemp");
+const forecastTime = document.querySelectorAll(".forecastTime");
 
 // 도시 이름을 입력하고, Enter키를 누루면 아래 함수가 실행됩니다.
 searchForm.addEventListener("submit",(event) => {
@@ -105,17 +111,12 @@ function getForecastInfo(appInfo) {
          `https://api.openweathermap.org/data/2.5/forecast?lat=${appInfo["lat"]}&lon=${appInfo["lng"]}&APPID=${WEATHER_KEY}`
       ).then((response) => {
          response.json().then((data) => {
-
-            // 12:00:00 중 ("12")시에 해당하는 부분들을 배열로 담았다.
-            listArr=data["list"].map(item=>(
-                  item["dt_txt"].split(" ")[1].split(":")[0]
-               )).slice();
-
+            console.log(data);
             //출력하고 싶은 일기예보 5개만 담았다.(현재시간의 예보를 제외한 다음 예보부터)
             for(let i=3; i<8;i++){
-               forecast.push(listArr[i]);
+               listArr.push(data["list"][i]);
             }
-            appInfo["forecast"] = forecast.slice();
+            appInfo["forecast"] = listArr.slice();
             console.log(appInfo);
 
             resolve(appInfo);
@@ -137,16 +138,36 @@ function setInfo(cityName, appInfo) {
                      appInfo["timeFormat"][1]+"분 ";
    
    //날씨 아이콘
-   const weatherLink = imgLinks[appInfo.weather];
+   const weatherLink = imgLinks[appInfo["weather"]];
    icon.src = `image/icon/${weatherLink}`; 
 
    //온도 섭씨->켈빈 °C = K − 273.15
-   temp.innerText = Math.floor(appInfo.temp-273.15)+" ℃";
+   temp.innerText = Math.floor(appInfo["temp"]-273.15)+" ℃";
 
    //도시 이름
    city.innerText = cityName;
 
+   //일기예보
+   for(let i=0; i<5; i++){
+      let forecastLink = imgLinks[appInfo["forecast"][i]["weather"][0]["main"]];
+      forecastIcon[i].src = `image/icon/${forecastLink}`;
+      forecastTemp[i].innerText = Math.floor(appInfo["forecast"][i]["main"]["temp"]-273.15)+"℃";
+      forecastTime[i].innerText = appInfo["forecast"][i]["dt_txt"].split(" ")[1].slice(0,2)+"시";
+   }
+
+   //날씨에 따른 배경 전환
+   const bgImgLink = bgImgLinks[appInfo["weather"]];
+   body.style.cssText = `background-image: url("image/background/${bgImgLink}")`;
+
+   //날씨에 따른 일기예보 배경 전환
+   forecastItems.forEach((item,i) => {
+      item.style.cssText = bgColorLinks[appInfo["forecast"][i]["weather"][0]["main"]];
+   });
     //입력창을 활성화 시킴.
    input.disabled = false;
 }
 
+// 12:00:00 중 ("12")시에 해당하는 부분들을 배열로 담았다.
+//  listArr=data["list"].map(item=>(
+//    item["dt_txt"].split(" ")[1].split(":")[0]
+// )).slice();
--- weatherApp/data.js
+++ weatherApp/data.js
@@ -4,3 +4,17 @@ const imgLinks = {
    Snow: "snowing.png",
    Clouds: "cloudy.png"
  };
+
+ const bgColorLinks = {
+   Clear : "background: linear-gradient(135deg, #FFFFFF 0%, #6DD5FA 50%, #83ED56 100%);",
+   Rain : "background: linear-gradient(135deg, #D8D8D8 0%, #B7B7B7 50%, #727272 100%);",
+   Snow : "background: linear-gradient(135deg, #FFFFFF 0%, #EFEFEF 50%, #E0E0E0 100%);",
+   Clouds : "background: linear-gradient(135deg, #CECECE 0%, #55B6FA 50%, #2446ED 100%);"
+ }
+
+ const bgImgLinks = {
+   Clear : "Clear.jpg",
+   Rain : "Rain.jpg",
+   Snow : "Snow.jpg",
+   Clouds : "Clouds.jpg"
+ }
